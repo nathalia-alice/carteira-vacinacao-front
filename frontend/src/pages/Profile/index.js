a9885f1f1d2166c35fad812a385d55e795a39d90
nathalia-alice <nathalia.alice.silv@gmail.com>
2020-05-16 01:39:24 -0300

    Adicionando botão sair

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { FiPower } from 'react-icons/fi';
+
 import api from '../../services/api';
 import './styles.css'
 import logoImg from  '../../assets/saúde.png';
@@ -8,7 +10,7 @@ var loadingVacinas = false;
 
 export default function Profile(){
     const [users, setUsers] = useState([]);
-
+    const history = useHistory();
     useEffect(()=>{
         if(loadingVacinas === false){
             api.get('vacinasxusuario')
@@ -19,6 +21,11 @@ export default function Profile(){
         }
     })
 
+    function handleLogout(){
+        localStorage.removeItem('token');
+        history.push('/')
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -28,7 +35,9 @@ export default function Profile(){
                 <Link className="button" to="/vacinasxusuario/new">
                    Cadastrar nova vacina
                 </Link>
-
+                <button type="button" onClick={handleLogout}>
+                    <FiPower size={18} color="#E02041"></FiPower>
+                </button>
             </header>
            <h1>Vacinas cadastradas</h1>
            <ul>
